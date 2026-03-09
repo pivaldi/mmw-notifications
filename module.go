2b353c8632289102e48d7cfb5d93adf417a1861a
pivaldi <git@ivaldi.me>
2026-03-09 02:14:47 +0100

    first commit

---
+++ module.go
@@ -0,0 +1,58 @@
+// services/notifications/module.go
+package notifications
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/ovya/ogl/core"
+	"github.com/rotisserie/eris"
+)
+
+type Module struct {
+	subscriber message.Subscriber
+	logger     *slog.Logger
+}
+
+// Ensure Module implements core.Module
+var _ core.Module = (*Module)(nil)
+
+func (m *Module) RegisterRoutes(_ *http.ServeMux) {}
+
+func Build(subscriber message.Subscriber, logger *slog.Logger) *Module {
+	return &Module{
+		subscriber: subscriber,
+		logger:     logger,
+	}
+}
+
+// StartWorkers boots up the background listeners for this module
+// This mehod blocks until shutdown (ctx canceled)
+func (m *Module) StartWorkers(ctx context.Context) error {
+	messages, err := m.subscriber.Subscribe(ctx, "todo.created")
+	if err != nil {
+		return eris.Wrap(err, "events subscription failed")
+	}
+
+	m.logger.Info("notification worker started listening for events")
+
+	// This blocks the errgroup correctly until shutdown
+	for msg := range messages {
+		m.logger.Info(fmt.Sprintf("Notification Service: received event new Todo! Payload: %s\n", string(msg.Payload)))
+
+		msg.Ack()
+	}
+
+	m.logger.Info("notification worker stopped gracefully")
+
+	return nil
+}
+
+func (m *Module) Close() error {
+	m.logger.Info("shutting down notifications module internal resources")
+
+	return nil
+}
